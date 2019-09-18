0a06c7c9a16cdbe6f980f86315916b515fd8e6ce
rzambranohbtn <1244@holbertonschool.com>
2019-09-18 17:48:55 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+*main - initial point
+*Return: whatever
+*Programming
+*is like building
+*a multilingual puzzle
+*/
 int main(void)
 {
 char *string = ("\"Programming is like building a multilingual puzzle");
