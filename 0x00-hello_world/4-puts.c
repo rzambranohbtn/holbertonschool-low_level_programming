57a47ea800de063a493905b2919f0f881fc5ccf4
rzambranohbtn <1244@holbertonschool.com>
2019-09-18 17:35:17 +0000

    5thchecking

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
- 
 int main(void)
 {
-  char *string = ("\"Programming is like building a multilingual puzzle");
-   puts(string);
-   return 0;
+char *string = ("\"Programming is like building a multilingual puzzle");
+puts(string);
+return (0);
 }
