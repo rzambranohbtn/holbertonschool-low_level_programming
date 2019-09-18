8c7d10e278c66571ef6a9bdebfdd8c17fb70b5cf
rzambranohbtn <1244@holbertonschool.com>
2019-09-18 19:05:30 +0000

    6-sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#include <studio.h>
+#include <stdio.h>
+/**
+*main- size of various types
+*on the computer
+*
+*Return: 0 for success
+*/
+int main (void)
+{
+printf("\nSize of a char: %d byte(s)",sizeof(char*));
+printf("\nSize of an int: %d byte(s)",sizeof(int*));
+printf("\nSize of a long int: %d byte(s)",sizeof(long int*));
+printf("\nSize of a long long int: %d byte(s)",sizeof(long long int*));
+printf("\nSize of a float: %d\n byte(s) byte(s)",sizeof(float*));
+return(0);
+}
