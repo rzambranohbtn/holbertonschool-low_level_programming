5eda51e2f4ff8c01e9850efcb1a64c4c87888698
rzambranohbtn <1244@holbertonschool.com>
2019-09-18 18:07:05 +0000

    5-printf-1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-char *grammar = ("with proper grammar, but the outcome is a piece of art");
-printf('%s\n', grammar);
+char *grammar = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", grammar);
 return (0);
 }
