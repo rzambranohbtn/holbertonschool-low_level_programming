c0f3fa723f85737874f4d65c5bdd5e8c19b19fe5
rzambranohbtn <1244@holbertonschool.com>
2019-09-19 19:06:20 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - initial point
+*Return: 0
+*alphabet game
+*/
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+printf("%i", x);
+putchar('\n');
+return (0);
+}
