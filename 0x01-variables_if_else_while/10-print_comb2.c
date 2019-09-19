242cb060e2beb3e8c305964de07174bc8dc4ff3a
rzambranohbtn <1244@holbertonschool.com>
2019-09-19 21:35:31 +0000

    strugle

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - initial point
+*Return: 0
+*patience
+*/
+int main(void)
+{
+	int i, j;
+	i = 48;
+	j = 48;
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 57 ||  j != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+
+		}
+		j = 48;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
