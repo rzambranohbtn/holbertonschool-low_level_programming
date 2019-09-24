bbbcdfeebe746310b22f62f5d59e03597523affa
rzambranohbtn <1244@holbertonschool.com>
2019-09-24 13:38:34 +0000

    last-digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 /**
- *@a - the number
- *print_last_digit - the last digit
+ * print_last_digit - prints the last digit
+ * @a: number
+ * Return: last
  */
 int print_last_digit(int a)
 {
 	int last;
+
 	last = a % 10;
 	if (last <= 0)
 		last = -1 * last;
 	_putchar(last + '0');
-	return(last);
+
+	return (last);
 }
