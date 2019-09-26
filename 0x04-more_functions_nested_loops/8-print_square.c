25e6c797b43606742c118425584baeba776db85c
rzambranohbtn <1244@holbertonschool.com>
2019-09-26 18:39:48 +0000

    8th

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_square - square
+ *@size: entry
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+			if (size == 0)
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
