fe1a9b898b70a271ecddb369b88ce33bc94f513d
rzambranohbtn <1244@holbertonschool.com>
2019-10-01 13:30:39 +0000

    last2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,9 +6,10 @@
 void puts_half(char *str)
 {
 	int counter, x, n;
-	counter=0;
-	x=0;
-	n=0;
+
+	counter = 0;
+	x = 0;
+	n = 0;
 
 	while (str[counter] != '\0')
 		counter++;
@@ -19,7 +20,7 @@ void puts_half(char *str)
 	else
 		n = (counter - 1) / 2;
 
-	for (x=n; x<counter; x++)
+	for (x = n; x < counter; x++)
 	{
 		_putchar(str[x]);
 	}
