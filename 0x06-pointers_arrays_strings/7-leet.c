c16e9417899848d4c762fcaa8eb98156de055b25
rzambranohbtn <1244@holbertonschool.com>
2019-10-03 22:19:30 +0000

    7th

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * leet - replace
+ * @s: string to encode
+ *
+ * Return: replace char by numbers
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char x[] = "aAeEoOtTlL";
+	char y[] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (x[j] == s[i])
+				s[i] = y[j];
+		}
+	}
+	return (s);
+}
