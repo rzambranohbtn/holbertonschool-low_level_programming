5a494456d9b0b97ba449f0ac638fa03b49450d57
Ricardo Zambrano <1244@holbertonschool.com>
2019-10-05 02:00:41 +0000

    new-review

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,8 +12,11 @@ char *_strcpy(char *dest, char *src)
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-	}
-	dest[i + 1] = '\0';
 
+		if (src[i + 1] == '\0')
+		{
+			dest[i + 1] = '\0';
+		}
+	}
 	return (dest);
 }
