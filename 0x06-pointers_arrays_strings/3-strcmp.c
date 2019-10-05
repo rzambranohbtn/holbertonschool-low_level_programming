440e3e05e2e8a36f4682cb54eb90cee7d48207e2
Ricardo Zambrano <1244@holbertonschool.com>
2019-10-05 03:27:31 +0000

    3th

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int rest;
 
-	if ((s1[i] != '\0' && s2[i] != '\0') || (s1[i] != s2[i]))
+	if ((s1[i] != '\0' || s2[i] != '\0') || (s1[i] != s2[i]))
 	{
 		rest = s1[i] - s2[i];
 	}
